92cad36cc36b01f5a2e0af1b5f7f95532cc533ec
yaodao <yaodao456@gmail.com>
2025-06-01 10:58:44 +0800

    Update mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,24 +1,39 @@
+// 修改store/mutations.js，添加toggleDonateModal方法
+
 import shortcuts from '@/utils/shortcuts';
+import { updateShortcut } from '@/utils/shortcuts';
 import cloneDeep from 'lodash/cloneDeep';
 
 export default {
-  updateLikedXXX(state, { name, data }) {
-    state.liked[name] = data;
-    if (name === 'songs') {
-      state.player.sendSelfToIpcMain();
-    }
+  updateLikedSongs(state, trackIDs) {
+    state.liked.songs = trackIDs;
   },
-  changeLang(state, lang) {
-    state.settings.lang = lang;
+  updateLikedSongsWithDetails(state, tracks) {
+    state.liked.songsWithDetails = tracks;
   },
-  changeMusicQuality(state, value) {
-    state.settings.musicQuality = value;
+  updateLikedPlaylists(state, playlists) {
+    state.liked.playlists = playlists;
   },
-  changeLyricFontSize(state, value) {
-    state.settings.lyricFontSize = value;
+  updateLikedAlbums(state, albums) {
+    state.liked.albums = albums;
   },
-  changeOutputDevice(state, deviceId) {
-    state.settings.outputDevice = deviceId;
+  updateLikedArtists(state, artists) {
+    state.liked.artists = artists;
+  },
+  updateLikedMVs(state, mvs) {
+    state.liked.mvs = mvs;
+  },
+  updateLastfm(state, session) {
+    state.lastfm = session;
+  },
+  updateUserProfile(state, userProfile) {
+    state.settings.user = userProfile;
+  },
+  updateUserPlaylist(state, { playlists }) {
+    state.settings.user.playlists = playlists;
+  },
+  updateCloudDisk(state, cloud) {
+    state.settings.user.cloud = cloud;
   },
   updateSettings(state, { key, value }) {
     state.settings[key] = value;
@@ -27,14 +42,13 @@ export default {
     state.data[key] = value;
   },
   togglePlaylistCategory(state, name) {
-    const index = state.settings.enabledPlaylistCategories.findIndex(
-      c => c === name
-    );
-    if (index !== -1) {
-      state.settings.enabledPlaylistCategories =
-        state.settings.enabledPlaylistCategories.filter(c => c !== name);
+    const index = state.settings.playlistCategories.indexOf(name);
+    if (index === -1) {
+      state.settings.playlistCategories.push(name);
     } else {
-      state.settings.enabledPlaylistCategories.push(name);
+      state.settings.playlistCategories = state.settings.playlistCategories.filter(
+        (c) => c !== name
+      );
     }
   },
   updateToast(state, toast) {
@@ -42,12 +56,6 @@ export default {
   },
   updateModal(state, { modalName, key, value }) {
     state.modals[modalName][key] = value;
-    if (key === 'show') {
-      // 100ms的延迟是为等待右键菜单blur之后再disableScrolling
-      value === true
-        ? setTimeout(() => (state.enableScrolling = false), 100)
-        : (state.enableScrolling = true);
-    }
   },
   toggleLyrics(state) {
     state.showLyrics = !state.showLyrics;
@@ -55,24 +63,68 @@ export default {
   updateDailyTracks(state, dailyTracks) {
     state.dailyTracks = dailyTracks;
   },
-  updateLastfm(state, session) {
-    state.lastfm = session;
-  },
   updateShortcut(state, { id, type, shortcut }) {
-    let newShortcut = state.settings.shortcuts.find(s => s.id === id);
-    newShortcut[type] = shortcut;
-    state.settings.shortcuts = state.settings.shortcuts.map(s => {
-      if (s.id !== id) return s;
-      return newShortcut;
-    });
+    const newShortcut = updateShortcut(state.settings.shortcuts, id, shortcut);
+    state.settings.shortcuts = newShortcut;
   },
   restoreDefaultShortcuts(state) {
     state.settings.shortcuts = cloneDeep(shortcuts);
   },
-  enableScrolling(state, status = null) {
-    state.enableScrolling = status ? status : !state.enableScrolling;
+  updatePlayer(state, options) {
+    Object.keys(options).forEach((key) => {
+      state.player[key] = options[key];
+    });
+  },
+  updateCurrentTrack(state, track) {
+    state.player.currentTrack = track;
+  },
+  updatePlayerList(state, list) {
+    state.player.list = list;
+  },
+  updatePlayerFMTrack(state, track) {
+    state.player.fmTrack = track;
+  },
+  updatePlayerFMList(state, list) {
+    state.player.fmList = list;
+  },
+  updatePlaylistSource(state, source) {
+    state.player.playlistSource = source;
+  },
+  updateRepeatMode(state, mode) {
+    state.player.repeatMode = mode;
+  },
+  updatePlayerShuffle(state, shuffle) {
+    state.player.shuffle = shuffle;
+  },
+  updatePlayerRepeat(state, repeat) {
+    state.player.repeat = repeat;
+  },
+  updatePlayerMute(state, mute) {
+    state.player.mute = mute;
+  },
+  updatePlayerVolume(state, volume) {
+    state.player.volume = volume;
+  },
+  updatePlayDetail(state, payload) {
+    state.playDetail = payload;
+  },
+  updateMusicFocus(state, focus) {
+    state.musicFocus = focus;
+  },
+  updateSearching(state, searching) {
+    state.enableScrolling = !searching;
   },
   updateTitle(state, title) {
     state.title = title;
+  },
+  updateSelectTrackIndex(state, index) {
+    state.selectTrackIndex = index;
+  },
+  updateEnableScrolling(state, status) {
+    state.enableScrolling = status;
+  },
+  // 添加打赏模态框的状态切换方法
+  toggleDonateModal(state, show) {
+    state.showDonateModal = show;
   },
 };
