790656ca13de12062cdebcf5c3aa296309a0b863
yaodao <yaodao456@gmail.com>
2025-06-01 10:59:26 +0800

    Update state.js

--- src/store/state.js
+++ src/store/state.js
@@ -1,19 +1,18 @@
 import initLocalStorage from './initLocalStorage';
 import pkg from '../../package.json';
 import updateApp from '@/utils/updateApp';
-
 if (localStorage.getItem('appVersion') === null) {
   localStorage.setItem('settings', JSON.stringify(initLocalStorage.settings));
   localStorage.setItem('data', JSON.stringify(initLocalStorage.data));
   localStorage.setItem('appVersion', pkg.version);
 }
-
 updateApp();
-
 export default {
   showLyrics: false,
   enableScrolling: true,
   title: 'YesPlayMusic',
+  // 添加打赏模态框的状态
+  showDonateModal: false,
   liked: {
     songs: [],
     songsWithDetails: [], // 只有前12首
